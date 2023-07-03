6434a603951e2819b99ef5fc6a49e7c472b31fe4
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-07-03 23:33:39 +0300

    correct js8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,17 +3,20 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 const gallery = document.querySelector('.gallery');
-galleryItems.forEach(item => {
-  const listItem = document.createElement('li');
-  const link = document.createElement('a');
-  link.href = item.original;
-  const image = document.createElement('img');
-  image.src = item.preview;
-  image.alt = item.description;
-  link.appendChild(image);
-  listItem.appendChild(link);
-  gallery.appendChild(listItem);
-});
+const galleryAdd = galleryItems
+  .map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join('');
 // Change code below this line
-const lightbox = new SimpleLightbox('.gallery a');
+const lightbox = new SimpleLightbox('.gallery a'), {};
 console.log(galleryItems);
